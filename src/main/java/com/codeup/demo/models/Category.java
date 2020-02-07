3200407f9387958f4fe3c5b6dc1589b17bab9793
RiverRobins <rivergrahamrobins@gmail.com>
2020-02-07 10:09:19 -0600

    added category

---
+++ src/main/java/com/codeup/demo/models/Category.java
@@ -0,0 +1,43 @@
+package com.codeup.demo.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    public Category(){}
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Category(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
