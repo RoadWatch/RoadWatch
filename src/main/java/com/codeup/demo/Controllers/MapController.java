4352432b0e186964ec50aeee03ba8d0d7427d76d
Amber Jones <amberlovescats14@gmail.com>
2020-02-07 11:22:16 -0600

    user exception

---
+++ src/main/java/com/codeup/demo/Controllers/MapController.java
@@ -0,0 +1,15 @@
+package com.codeup.demo.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MapController {
+
+    @GetMapping("/map")
+    public String showMapPage(Model model){
+        return "map/index";
+    }
+
+}
