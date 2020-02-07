3a7af0ddc4f82a10b92ecad9e32c6506db43ac39
Amber Jones <amberlovescats14@gmail.com>
2020-02-07 12:20:57 -0600

    setting up security classes

--- src/main/java/com/codeup/demo/Controllers/UserController.java
+++ src/main/java/com/codeup/demo/Controllers/UserController.java
@@ -3,21 +3,26 @@ package com.codeup.demo.Controllers;
 import com.codeup.demo.Repos.Users;
 import com.codeup.demo.exception.UserException;
 import com.codeup.demo.models.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
     private Users userDao;
+    private PasswordEncoder passwordEncoder;
 
-    public UserController(Users userDao) {
+    public UserController(Users userDao, PasswordEncoder passwordEncoder) {
         this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    //! USER PROFILE
+    //! USER PROFILE, get by id
     @GetMapping("/user/{id}")
     public String showUserProfile(
             @PathVariable long id,
@@ -28,7 +33,55 @@ public class UserController {
         model.addAttribute("user", user);
         return "user/profile";
     }
+    //! CREATE USER
+    @GetMapping("/user/create")
+    public String showCreateUser(Model model){
+        model.addAttribute("user", new User());
+        return "user/create";
+    }
 
+    @PostMapping("/user/create")
+    public String createUser(
+            @ModelAttribute User user
+    ) {
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setEmail(hash);
+        userDao.save(user);
+        return "redirect:/user/" + user.getId();
+    }
+
+    //! EDIT USER PROFILE
+    @GetMapping("/user/{id}/edit")
+    public String showEditUserPage(
+            @PathVariable long id,
+            Model model
+    ) throws UserException {
+        User user = userDao.findById(id)
+                .orElseThrow(()-> new UserException());
+        model.addAttribute("user", user);
+        return "user/edit";
+    }
+
+    @PostMapping("/user/{id}/edit")
+    public String editUserProfile(
+            @PathVariable long id,
+            @ModelAttribute User user
+    ){
+        userDao.save(user);
+        return "redirect:/user/"+user.getId();
+    }
+
+    //! DELETE USER
+    @PostMapping("/user/{id}/delete")
+    public String deleteUser(
+            @PathVariable long id
+    ) throws UserException {
+        User user = userDao.findById(id)
+                .orElseThrow(()-> new UserException());
+
+        return "redirect:/login";
+
+    }
 
 
 }
