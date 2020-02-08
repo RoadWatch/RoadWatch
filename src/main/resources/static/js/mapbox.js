f4a3208c95f79b9e76551c9cdd7453b7cb054425
Christopher Aguirre <christopher.aguirre210@gmail.com>
2020-02-08 11:20:32 -0600

    got the map back on the /map page

---
+++ src/main/resources/static/js/mapbox.js
@@ -0,0 +1,67 @@
+"use strict";
+
+
+mapboxgl.accessToken = mapboxToken;
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    zoom: 15,
+    center: [-98.485352, 29.423618]
+    // pitch: 45
+});
+
+var markerOptions = {
+    color: "#038f07",
+    draggable: true
+};
+
+
+function geocode(search, token) {
+    var baseUrl = 'https://api.mapbox.com';
+    var endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+        .then(function(res) {
+            return res.json();
+            // to get all the data from the request, comment out the following three lines...
+        }).then(function(data) {
+            return data.features[0].center;
+        });
+}
+
+
+var restaurants = [
+    {
+        name: "Spaghetti Warehouse",
+        location: "1226 E Houston St, San Antonio, TX 78205",
+        description: "It's a good spaghetti!"
+    },
+    {
+        name: "Mexican Manhattan",
+        location: "110 Soledad St, San Antonio, TX 78205",
+        description: "Lovely margaritas!"
+    },
+    {
+        name: "River Center Mall",
+        location: "849 E Commerce St, San Antonio, TX 78205",
+        description: "The food court is pretty diverse."
+    }
+];
+
+
+restaurants.forEach(function (restaurant,index) {
+    geocode(restaurant.location, mapboxToken).then(function (result) {
+
+
+        var pops = new mapboxgl.Popup()
+            .setLngLat(result)
+            .setHTML("<em><h2>"+restaurant.name+"</em></h2>" + "<br>" + restaurant.location + "<br>" + restaurant.description)
+            .addTo(map);
+        var marker = new mapboxgl.Marker(markerOptions)
+            .setLngLat(result)
+            .setPopup(pops)
+            .togglePopup()
+            .addTo(map);
+        console.log("This is " + index + " foods!");
+    });
+});
