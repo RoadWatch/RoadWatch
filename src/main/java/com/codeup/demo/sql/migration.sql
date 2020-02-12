83dcb92a0c5e5db2a3484e2e0cb5dd6c7fda89b8
Christopher Aguirre <christopher.aguirre210@gmail.com>
2020-02-12 07:50:36 -0600

    added the database I started off with and added links to the footer

--- src/main/java/com/codeup/demo/sql/migration.sql
+++ src/main/java/com/codeup/demo/sql/migration.sql
@@ -1,6 +1,7 @@
-drop database road_watch;
-show databases ;
-use road_watch;
+drop database road_conditions;
+
+show databases;
+use road_conditions;
 show tables;
 
 describe users;
@@ -9,3 +10,16 @@ select * from users;
 select * from posts;
 
 describe posts;
+
+
+
+##----Chris' database (I know it's wrong but whatevs)
+drop database spring_blog;
+
+use spring_blog;
+
+select * from users;
+
+show tables;
+
+##----
