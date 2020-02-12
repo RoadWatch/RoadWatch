2150008c7c6747f4670b23d95b15f67b47a8a946
Amber Jones <amberlovescats14@gmail.com>
2020-02-12 16:55:24 -0600

    working on geocode

--- src/main/java/com/codeup/demo/Controllers/MapController.java
+++ src/main/java/com/codeup/demo/Controllers/MapController.java
@@ -3,6 +3,7 @@ package com.codeup.demo.Controllers;
 import com.codeup.demo.Repos.Reports;
 import com.codeup.demo.models.Report;
 import com.codeup.demo.services.EnviromentSvc;
+import com.codeup.demo.services.SearchSvc;
 import com.mapbox.api.geocoding.v5.MapboxGeocoding;
 import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -17,10 +20,12 @@ import java.util.List;
 public class MapController {
     private EnviromentSvc enviromentSvc;
     private Reports reportsDao;
+    private SearchSvc searchSvc;
 
-    public MapController(EnviromentSvc enviromentSvc, Reports reportsDao) {
+    public MapController(EnviromentSvc enviromentSvc, Reports reportsDao, SearchSvc searchSvc) {
         this.enviromentSvc = enviromentSvc;
         this.reportsDao = reportsDao;
+        this.searchSvc = searchSvc;
     }
 
     @GetMapping("/map")
@@ -33,7 +38,11 @@ public class MapController {
     }
 
     @PostMapping("/report/add")
-    public String addReport(){
+    public String addReport(
+            @RequestParam String query
+    ){
+        String token = enviromentSvc.getMapboxKey();
+        searchSvc.executeSearch(query, token);
         return "redirect:/map";
     }
 
--- src/main/java/com/codeup/demo/services/SearchSvc.java
+++ src/main/java/com/codeup/demo/services/SearchSvc.java
@@ -1,9 +1,16 @@
 package com.codeup.demo.services;
 
 import com.mapbox.api.geocoding.v5.MapboxGeocoding;
+import com.mapbox.api.geocoding.v5.models.CarmenFeature;
+import com.mapbox.api.geocoding.v5.models.GeocodingResponse;
+import com.mapbox.geojson.Point;
+import org.springframework.stereotype.Service;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-import javax.security.auth.callback.Callback;
-
+import java.util.List;
+@Service
 public class SearchSvc {
     private String query;
     private String token;
@@ -18,7 +25,21 @@ public class SearchSvc {
                         .accessToken(this.token)
                         .query("codeup")
                         .build();
+        mapboxGeocoding.enqueueCall(new Callback<GeocodingResponse>() {
+            @Override
+            public void onResponse(Call<GeocodingResponse> call, Response<GeocodingResponse> response) {
+                List<CarmenFeature> results = response.body().features();
+                if(results.size() > 0){
+                    Point center = results.get(0).center();
+                    System.out.println("CENTER"+center);
+                }
+            }
 
+            @Override
+            public void onFailure(Call<GeocodingResponse> call, Throwable throwable) {
+
+            }
+        });
 
     }
 
