dbe4eaca8f2f92c3be60af332ab5593f90a531c1
RiverRobins <rivergrahamrobins@gmail.com>
2020-02-13 09:36:49 -0600

    I did a thing

--- src/main/java/com/codeup/demo/Controllers/MapController.java
+++ src/main/java/com/codeup/demo/Controllers/MapController.java
@@ -36,13 +36,9 @@ public class MapController {
     @GetMapping("/map")
     public String showMapPage(Model model){
         List<Report> userReports = reportsDao.findAll();
-        if (!(userReports.size() <= 0 || userReports == null)) {
+        if (userReports != null) {
             model.addAttribute("userReports", userReports);
         }
-//        List<Report> userReports = reports.findAll();
-//        if (!(userReports.size() <= 0 || userReports == null)) {
-//            model.addAttribute("userReports", userReports);
-//        }
         return "map/index";
     }
 
